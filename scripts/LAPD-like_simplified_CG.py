8991accb538ebee8db231ad448139846fa3e8543
Owen Parry <owen.parry@ukaea.uk>
2024-07-03 14:58:47 +0100

    Collect params.

--- scripts/LAPD-like_simplified_CG.py
+++ scripts/LAPD-like_simplified_CG.py
@@ -1,8 +1,8 @@
-# LAPD-like_CG_v2.py
+# LAPD-like_simplified_CG.py
 # Attempt at time-dependent solver of LAPD-like equations, with upwind flux
 # based upon SOL_3D_DG_upwind_tdep_irksome_dev.py (longitudinal dynamics)
 # and Nektar-Driftwave_port_irksome_STFC_v2_working_submit.py (transverse dynamics)
-# this version fixes wrong equation for longitudial velocity cpt
+# this version fixes wrong equation for longitudinal velocity cpt
 
 # fmt: off
 from firedrake import as_vector, BoxMesh, Constant, DirichletBC, div, dot, dx,\
@@ -14,9 +14,23 @@ import math
 from irksome import Dt, GaussLegendre, TimeStepper
 import time
 
-meshres = 64
-h = 1.0 / meshres
-mesh = BoxMesh(16, meshres, meshres, 2.0, 0.2, 0.2, hexahedral=True)
+# ================================ User options ================================
+# mesh
+nx = 16
+ny_nz = 64
+Lx = 2.0
+Ly_Lz = 0.2
+use_hex_mesh = True
+
+# time (4.0 / 100 is the standard for longitudinal-only)
+T = 4.0
+timeres = 200
+
+output_base = "LAPD-like_CG_v2"
+# ==============================================================================
+
+h = 1.0 / ny_nz
+mesh = BoxMesh(nx, ny_nz, ny_nz, Lx, Ly_Lz, Ly_Lz, hexahedral=use_hex_mesh)
 
 V1 = FunctionSpace(mesh, "CG", 1)  # n
 V2 = FunctionSpace(mesh, "CG", 1)  # u - velocity x-cpt
@@ -24,9 +38,6 @@ V3 = FunctionSpace(mesh, "CG", 1)  # w
 V = V1 * V2 * V3
 V4 = FunctionSpace(mesh, "CG", 1)  # phi
 
-# time parameters (4.0 / 100 is the standard for longitudinal-only)
-T = 4.0
-timeres = 200
 t = Constant(0.0)
 dt = Constant(T / timeres)
 
@@ -53,9 +64,9 @@ phi_s = Function(V4)
 
 # source function, amplitude was simple cranked up until nontrivial behaviour was seen
 nstarFunc = Function(V1)
-nstarFunc.interpolate(nstar*0.0 + 100.0*exp(-((x[1]-0.1)**2+(x[2]-0.1)**2)/(2*width_T**2)))  # fmt: skip
+nstarFunc.interpolate(nstar*0.0 + 100.0*exp(-((x[1]-0.1)**2+(x[2]-Ly_Lz/2)**2)/(2*width_T**2)))  # fmt: skip
 
-outpath_ICs = "LAPD-like_CG_v2_init.pvd"
+outpath_ICs = f"{output_base}_init.pvd"
 PETSc.Sys.Print("Writing ICs to ", outpath_ICs)
 VTKFile(outpath_ICs).write(nuw.sub(0), nuw.sub(1), nuw.sub(2))
 
@@ -82,11 +93,10 @@ params = {
 }
 
 # Dirichlet BCs are needed for boundary velocity
-
 bc_outflow_1 = DirichletBC(V.sub(1), -1, 1)
 bc_outflow_2 = DirichletBC(V.sub(1), 1, 2)
 
-bc_n = DirichletBC(V.sub(0), 0.0, [3, 4, 5, 6])
+# bc_n = DirichletBC(V.sub(0), 0.0, [3, 4, 5, 6])
 
 stepper = TimeStepper(F, butcher_tableau, t, dt, nuw, solver_parameters=params, bcs=[bc_outflow_1, bc_outflow_2])  # fmt: skip
 
@@ -110,7 +120,7 @@ nullspace = VectorSpaceBasis(constant=True)
 
 # end of stuff for elliptic solve
 
-outfile = VTKFile("LAPD-like_CG_v2.pvd")
+outfile = VTKFile(f"{output_base}.pvd")
 
 start = time.time()
 
@@ -118,7 +128,12 @@ start = time.time()
 PETSc.Sys.Print(f"Using:")
 PETSc.Sys.Print(f" dt      = {float(dt):.5g}")
 PETSc.Sys.Print(f" T_end   = {float(T):.5g}")
-PETSc.Sys.Print(f" meshres = {meshres:d}")
+PETSc.Sys.Print(f" transverse:")
+PETSc.Sys.Print(f"   size  = {ny_nz:d}")
+PETSc.Sys.Print(f"   res   = {Ly_Lz:.5g}")
+PETSc.Sys.Print(f" parallel:")
+PETSc.Sys.Print(f"   size  = {nx:d}")
+PETSc.Sys.Print(f"   res   = {Lx:.5g}")
 
 PETSc.Sys.Print("\nTimestep loop:")
 step = 0
