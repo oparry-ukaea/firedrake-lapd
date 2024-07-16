930781986ca945561c39a2ca6df3ccaf1b36321a
Owen Parry <oparry@ukaea.uk>
2024-07-16 16:01:56 +0100

    Check return codes in build.bash to avoid hiding errors.

--- hpc/archer2/singularity/build.bash
+++ hpc/archer2/singularity/build.bash
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+check_ret() {
+    ret_code="$?"
+    if [ $# -ne 1 ]; then
+        echo "check_ret called with $# args (rather than 1) "
+        exit 99 
+    fi
+    lbl=$1
+    if [ $ret_code -ne 0 ]; then
+        echo "$lbl failed with return code $ret_code"
+        exit $ret_code
+    fi
+}
+
 for cmd in "docker" "singularity"
 do
     if ! command -v "$cmd" &> /dev/null
@@ -14,13 +27,16 @@ echo "N.B. Pass --no-cache to this script to disable layer caching and force all
 # Build the docker images
 compose_cmd="docker compose build $*"
 eval "$compose_cmd"
+check_ret "docker compose build"
 
 # Convert to sandbox singularity
 singularity build --force \
    --sandbox ./firedrake-singularity \
    docker-daemon://firedrake:latest
+check_ret "singularity sandbox build"
 
 # Build singularity image
 singularity build --force \
 firedrake.sif \
 ./firedrake-singularity
+check_ret "singularity image build"
