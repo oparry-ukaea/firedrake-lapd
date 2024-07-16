eaf31b8dcde926339de498188fbb4939d12a30a4
Owen Parry <oparry@ukaea.uk>
2024-07-16 16:01:27 +0100

    Passthrough args for docker compose to make it easier to force rebuild.

--- hpc/archer2/singularity/build.bash
+++ hpc/archer2/singularity/build.bash
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Check docker and singularity are available
 for cmd in "docker" "singularity"
 do
     if ! command -v "$cmd" &> /dev/null
@@ -10,8 +9,11 @@ do
     fi
 done
 
+echo "N.B. Pass --no-cache to this script to disable layer caching and force all images to rebuild from scratch"
+
 # Build the docker images
-docker compose build
+compose_cmd="docker compose build $*"
+eval "$compose_cmd"
 
 # Convert to sandbox singularity
 singularity build --force \
