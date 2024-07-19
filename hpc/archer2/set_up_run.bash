bd4879ff1b6bbcbe6a06392f105de1de50ece1eb
Owen Parry <oparry@ukaea.uk>
2024-07-19 15:44:01 +0100

    Copy mesh file when setting up archer run.

--- hpc/archer2/set_up_run.bash
+++ hpc/archer2/set_up_run.bash
@@ -11,15 +11,16 @@ if [ -z "$ACCOUNT" ]; then
     exit 2
 fi
 
-script_name="LAPD-like_simplified_CG.py"
+script_names="LAPD-like_simplified_CG.py meshes.py"
 
 # Settings
 run_dir="${repo_root}/runs/${run_name}"
 archer_dir="${repo_root}/hpc/archer2"
-script="${repo_root}/scripts/${script_name}"
 rm -rf "${run_dir}"
 mkdir -p "${run_dir}"
 # Copy Slurm script, setting account, image directory
 sed "${archer_dir}/sub_fd-singularity.slurm" -e "s|\[SET_ACCOUNT_HERE\]|$ACCOUNT|" -e "s|\[SET_IMAGE_DIR_HERE\]|${repo_root}/hpc/archer2/singularity|" > "${run_dir}/sub_fd-singularity.slurm"
-# Copy python script
-cp "$script" "${run_dir}"
+# Copy python script(s)
+for script_name in $script_names; do
+    cp "${repo_root}/scripts/${script_name}" "${run_dir}"
+done
