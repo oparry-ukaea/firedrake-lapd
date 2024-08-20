5c0244cb6d48cf45a63bfaeffd9fe50cdd4b4005
Owen Parry <owen.parry@ukaea.uk>
2024-08-20 09:14:03 +0100

    First attempt at checkpointing.

--- scripts/nova_CG.py
+++ scripts/nova_CG.py
@@ -6,6 +6,7 @@
 # aspect ratio is like actual device but the beam might be wider than in actual device
 
 from firedrake import (
+    BoxMesh,
     CheckpointFile,
     Constant,
     as_vector,
@@ -30,27 +31,50 @@ import math
 from irksome import Dt, GaussLegendre, MeshConstant, TimeStepper
 import time
 
-# this is how to restart from checkpoint data.  You will need to comment out later definitions of these objects
-# and you will need to comment out initializations of any fields e.g. n and u
-# with CheckpointFile("LAPD_nova_100.h5", 'r') as infile:
-#    mesh_in  = infile.load_mesh("LAPD_nova_mesh")
-#    nuw_in   = infile.load_function(mesh_in, "nuw")
-#    phi_s_in = infile.load_function(mesh_in, "phi_s")
+# ================================== Options ==================================
+output_base = "lapd_nova"
+output_freq = 10
+chk_freq = 10  # Write one checkfile for every [chk_freq] output files
+restart_chknum = None  # Set to an integer to restart from checkpoint file
 
+# time parameters (takes about 50 time units to get longitudinal to equilibrium)
+T = 100.0
+timeres = 2000
+
+# transverse mesh resolution (need to make sure this is consistent with chk file if restarting from a checkpoint)
 meshres = 32
+# =============================================================================
+
+
 h_tran = 2.0 / meshres
 h_long = 20.0 / meshres
-mesh = BoxMesh(16, meshres, meshres, 20.0, 2.0, 2.0, hexahedral=True)
+
+# Set up/load mesh, set up function spaces, set up/load functions
+if restart_chknum is None:
+    mesh = BoxMesh(16, meshres, meshres, 20.0, 2.0, 2.0, hexahedral=True)
+else:
+    restart_fname = f"{output_base}_chk_{restart_chknum}.h5"
+    with CheckpointFile(restart_fname, "r") as infile:
+        mesh = infile.load_mesh(f"{output_base}_mesh")
+        nuw = infile.load_function(mesh, "nuw")
+        phi_s = infile.load_function(mesh, "phi_s")
 
 V1 = FunctionSpace(mesh, "CG", 1)  # n
 V2 = FunctionSpace(mesh, "CG", 1)  # u - velocity x-cpt
 V3 = FunctionSpace(mesh, "CG", 1)  # w
 V = V1 * V2 * V3
 V4 = FunctionSpace(mesh, "CG", 1)  # phi
+x = SpatialCoordinate(mesh)
 
-# time parameters (takes about 50 time units to get longitudinal to equilibrium)
-T = 100.0
-timeres = 2000
+if restart_chknum is None:
+    nuw = Function(V)
+    phi_s = Function(V4)
+    # initialize parallel velocity
+    nuw.sub(1).interpolate(0.1 * (x[0] - 10.0))
+    # background init density
+    nuw.sub(0).interpolate(1.0e-4)
+
+# Initial time and timestep
 t = Constant(0.0)
 dt = Constant(T / timeres)
 
@@ -64,16 +88,10 @@ width_T = (
     10 * 0.05
 )  # transverse width for Gaussian source (increased from 10*0.025 to make more stable, smaller gradients)
 
-# other options
-output_freq = 10
-
-x = SpatialCoordinate(mesh)
-nuw = Function(V)
 n, u, w = split(nuw)
 v1, v2, v3 = TestFunctions(V)
 phi = TrialFunction(V4)
 v4 = TestFunction(V4)
-phi_s = Function(V4)
 
 # source function, amplitude as in Rogers-Ricci
 nstarFunc = Function(V1)
@@ -82,7 +100,7 @@ nstarFunc.interpolate(
 )
 
 # TRIALCODE check init data
-# File("LAPD_nova_init.pvd").write(nuw.sub(0), nuw.sub(1), nuw.sub(2))
+# File(f"{output_base}_init.pvd").write(nuw.sub(0), nuw.sub(1), nuw.sub(2))
 # quit()
 
 # weak form
@@ -172,12 +190,6 @@ bc_outflow_2 = DirichletBC(V.sub(1), 1, 2)
 
 bc_n = DirichletBC(V.sub(0), 1.0e-4, [3, 4, 5, 6])
 
-# initialize parallel velocity
-nuw.sub(1).interpolate(0.1 * (x[0] - 10.0))
-
-# background init density
-nuw.sub(0).interpolate(1.0e-4)
-
 stepper = TimeStepper(
     F,
     butcher_tableau,
@@ -209,7 +221,7 @@ nullspace = VectorSpaceBasis(constant=True)
 
 # end of stuff for elliptic solve
 
-outfile = VTKFile("LAPD_nova.pvd")
+outfile = VTKFile(f"{output_base}.pvd")
 
 start = time.time()
 
@@ -232,6 +244,15 @@ while float(t) < float(T):
     phi_s.rename("potential")
     if cnt % output_freq == 0:
         outfile.write(nuw.sub(0), nuw.sub(1), nuw.sub(2), phi_s, p)
+        output_num = cnt / output_freq
+        if output_num % chk_freq == 0:
+            # Write HDF5 checkpoint
+            with CheckpointFile(
+                f"{output_base}_chk_{output_num}.h5", "w"
+            ) as chk_outfile:
+                chk_outfile.save_mesh(mesh)
+                chk_outfile.save_function(nuw)
+                chk_outfile.save_function(phi_s)
     print("t =")
     print(t)
     stepper.advance()
@@ -242,12 +263,6 @@ while float(t) < float(T):
 
 end = time.time()
 wall_time = end - start
-
-# HDF5 output for restart
-with CheckpointFile("LAPD_nova_100.h5", "w") as outfile:
-    outfile.save_mesh(mesh)
-    outfile.save_function(nuw)
-    outfile.save_function(phi_s)
 
 print("done.")
 print("\n")
