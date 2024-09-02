08abbe4940ff69614e8843967898ef50732a91e6
Owen Parry <owen.parry@ukaea.uk>
2024-09-02 13:28:10 +0100

    Clearer Poisson bracket func.

--- scripts/2Drogers-ricci_CG.py
+++ scripts/2Drogers-ricci_CG.py
@@ -2,6 +2,7 @@ from firedrake import (
     as_vector,
     Constant,
     DirichletBC,
+    div,
     dot,
     dx,
     exp,
@@ -99,7 +100,9 @@ def phi_solve_setup(phi_space, vorticity, cfg):
 
 
 def poisson_bracket(f, phi, B):
-    return Constant(1 / B) * (grad(phi)[0] * grad(f)[1] - grad(phi)[1] * grad(f)[0])
+    one_over_B = Constant(1 / B)
+    driftvel = as_vector([one_over_B * grad(phi)[1], -one_over_B * grad(phi)[0]])
+    return Constant(1 / B) * div(f * driftvel)
 
 
 def rogers_ricci2D():
